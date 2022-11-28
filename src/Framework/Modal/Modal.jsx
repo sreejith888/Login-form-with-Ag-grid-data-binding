2e994c1be03aa993eca6b98977165c9e271cba25
Sreejith <96437537+sreejith888@users.noreply.github.com>
2022-11-28 15:05:18 +0530

    Add files via upload

--- src/Framework/Modal/Modal.jsx
+++ src/Framework/Modal/Modal.jsx
@@ -1,49 +1,149 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useState } from "react";
 import "./Modal.css";
 
-function Modal({ setOpenModal }) {
+function Modal(props) {
+  const [formValidationError, setFormValidationError] = useState({});
+  const [popupValues, setPopupValues] = useState({
+    txtName: "",
+    txtEmail: "",
+    txtAddress: "",
+  });
+  const[submitted,setSubmitted]=useState(false);
+
+
+  const clearForm = () => {
+    console.log("clear");
+    setPopupValues({
+      txtName: "",
+      txtEmail: "",
+      txtAddress: "",
+      
+    });
+  };
+
+
+  const validateInputField = (name, value) => {
+    let errorsMsg = "";
+    if (name === "txtName" || name === "txtAddress") {
+      if (!value || typeof value === "undefined" || value === null) {
+        errorsMsg = "Can not be empty";
+      }
+    } else if (name === "txtEmail") {
+      if (!value || typeof value === "undefined" || value === null) {
+        errorsMsg = "Email Address Cannot be empty";
+      } else if (value) {
+        const regex = new RegExp(/[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,15}/g);
+        if (!regex.test(value)) {
+          errorsMsg = "Invalid Email";
+        }
+      }
+    }
+    return errorsMsg;
+  };
+
+  const handleValidation = () => {
+    try {
+      const errors = {};
+      let formIsValid = true;
+      errors["txtName"] = validateInputField("txtName", popupValues.txtName);
+      errors["txtEmail"] = validateInputField("txtName", popupValues.txtEmail);
+      errors["txtAdress"] = validateInputField("txtName",popupValues.txtAddress);
+      if (Object.values(errors).join("").toString()) {
+        formIsValid = false;
+      }
+      setFormValidationError(errors);
+      return formIsValid;
+    } catch (error) {
+      alert(error);
+      return false;
+    }
+  };
+
+  const updatePopupValues = (name, value) => {
+    formValidationError[name] = validateInputField(name, value);
+    console.log(popupValues);
+    setPopupValues((Values) => ({
+      ...Values,
+      [name]: value,
+    }));
+  };
+
+
+  const saveData = async () => {
+    console.log("clicked");
+    if (!handleValidation()) {
+      console.log(formValidationError);
+      return;
+    }
+    const formData = {
+      main: {
+        name: popupValues && popupValues.txtName ? popupValues.txtName : "",
+        email: popupValues && popupValues.txtEmail ? popupValues.txtEmail : "",
+        address:popupValues && popupValues.txtAddress ? popupValues.txtAddress : "",
+      },
+    };
+    
+      
+        let UpdateData = {
+          name: formData.main.name,
+          email: formData.main.email,
+          body: formData.main.address,
+        };
+        props.UpdatestudentData(UpdateData);
+        console.log(props);
+    clearForm();
+    setSubmitted(true);
+  };
+  useEffect(()=>{
+    console.log(props);
+  },[])
   return (
     <div className="modalBackground">
       <div className="modalContainer">
         <div className="titleCloseBtn">
           <button
             className="btn1"
-            onClick={() => {
-              setOpenModal(false);
-            }}
-          >
-            X
+            onClick={() => {props.setOpenModal(false)}}
+          > X
           </button>
         </div>
         <div className="title">
           <h1>Popup Modal</h1>
         </div>
         <div className="body">
-          <input placeholder="Name" />
-          <input placeholder="Email" />
-          <input placeholder="Address" />
-          <input placeholder="Phone Number" />
+  
+          <input
+            type="text"
+            value={popupValues.email}
+            placeholder="Name"
+            name="txtName"
+            onChange={(e) => updatePopupValues(e.target.name, e.target.value)}
+          />
+           {formValidationError["txtName"]}
+          <input
+            type="text"
+            value={popupValues.address}
+            placeholder="Email"
+            name="txtEmail"
+            onChange={(e) => updatePopupValues(e.target.name, e.target.value)}
+          />
+          {formValidationError["txtEmail"]}
+          <input
+            type="text"
+            value={popupValues.phoneNumber}
+            placeholder="Address"
+            name="txtAddress"
+            onChange={(e) => updatePopupValues(e.target.name, e.target.value)}
+          />
+           {formValidationError["txtAddress"]}
         </div>
         <div className="footer">
-          <button
-            onClick={() => {
-              setOpenModal(false);
-            }}
-            id="cancelBtn"
-          >
-            Cancel
-          </button>
-          <button
-            onClick={() => {
-              setOpenModal(false);
-              alert("Response noted")
-            }}
-          >
-            Save
-          </button>
+          <button onClick={() => clearForm()}>Clear</button>
+          <button onClick={() => {saveData()
+          }}>Save</button>
         </div>
       </div>
-      /
     </div>
   );
 }
--- src/components/StudentsData/studentsData.jsx
+++ src/components/StudentsData/studentsData.jsx
@@ -7,42 +7,71 @@ import "./studentsData.css";
 import Modal from "../../Framework/Modal/Modal";
 
 function StudentsData() {
-  const [data, setData] = useState([]);
+  const [studentData, setStudentData] = useState([]);
   const [loading, setLoading] = useState(false);
   const [modalOpen, setModalOpen] = useState(false);
+  const [studentListGridApi, setStudentListGridApi] = useState([]);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      setLoading(true);
-      try {
-        const response = await axios.get(
-          "https://jsonplaceholder.typicode.com/comments"
-        );
-        setData(response.data);
-      } catch (errror) {
-        console.error(Error);
-      }
-      setLoading(false);
-    };
-    fetchData();
-  }, []);
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     setLoading(true);
+  //     try {
+  //       const response = await axios.get(
+  //         "https://jsonplaceholder.typicode.com/comments"
+  //       );
+  //       setStudentData(response.data);
+  //     } catch (errror) {
+  //       console.error(Error);
+  //     }
+  //     setLoading(false);
+  //   };
+  //   fetchData();
+  // }, []);
+
   const columnDefs = [
-    { headerName: "ID", field: "id", width: 200 },
-    { headerName: "Name", field: "name", width: 200 },
-    { headerName: "Email", field: "email", width: 200 },
-    { headerName: "Body", field: "body", width: 200 },
+    { headerName: "Sr. No.", field: "#", width: 80, valueGetter: "node.rowIndex + 1" },
+    { headerName: "Name", field: "name", width: 200,floatingFilter: true },
+    { headerName: "Email", field: "email", width: 200 ,floatingFilter: true},
+    { headerName: "Body", field: "body", width: 200 ,floatingFilter: true},
+  ];
+
+  const rowData = [
+    { name: "sreejith", email: "sreejith@gmail.com", body: "React developer" },
+    { name: "ajith", email: "aajith@gmail.com", body: "Js developer" },
+    { name: "ram", email: "ram@gmail.com", body: "Java developer" },
+    { name: "vikash", email: "vikash@gmail.com", body: "Python developer" },
   ];
   const defaultColDef = {
     sortable: true,
     flex: 1,
     filter: true,
-    floatingFilter: true,
+    editable: true,
   };
 
-  const openModel = () => {
-    setModalOpen(true);
-    console.log("clicked");
+  const onStudentListGridReady = (params) => {
+    setStudentListGridApi(params.api);
+    console.log(params.api);
   };
+
+  const UpdatestudentData = (data) => {
+    let OldData = studentData;
+    OldData.unshift(data);
+    setStudentData(OldData);
+    if (studentListGridApi) {
+      studentListGridApi.setRowData(studentData);
+    }
+  };
+
+  // const UpdateStatusFunc = () => {
+  //   let OldData = studentData;
+  //   OldData[rowIndex].ActiveStatus = type === "ACTIVE" ? "YES" : "NO";
+  //   setStudentData([]);
+  //   setStudentData(OldData);
+  //   if (studentListGridApi) {
+  //     studentListGridApi.setRowData(studentData);
+  //   }
+  // };
+
   return (
     <div className="StudentsData">
       <div className="head">
@@ -56,14 +85,20 @@ function StudentsData() {
           Add +
         </button>
 
-        {modalOpen && <Modal setOpenModal={setModalOpen} />}
+        {modalOpen && (
+          <Modal
+            setOpenModal={setModalOpen}
+            UpdatestudentData={UpdatestudentData}
+          />
+        )}
       </div>
 
       <div className="ag-theme-alpine" style={{ height: "600px" }}>
         <AgGridReact
           columnDefs={columnDefs}
-          rowData={data}
+          rowData={rowData}
           defaultColDef={defaultColDef}
+          onGridReady={onStudentListGridReady}
         ></AgGridReact>
       </div>
     </div>
